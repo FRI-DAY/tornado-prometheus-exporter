6626a40e81d2c90175f991586d3b3193791b8195
Sebastian Brandt <sebastian.brandt@friday.de>
2018-04-09 15:37:38 +0200

    CI: set up basic integration
    Travic CI calls tox calls pytest (only flake8 and isort so far)

--- setup.py
+++ setup.py
@@ -10,4 +10,14 @@ setup(
     author='Sebastian Brandt',
     author_email='sebastian.brandt@friday.de',
     description='Prometheus exporter for Tornado applications',
+    classifiers=(
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'),
 )
